d6d7a9c0a711a57602d659672c9fee6109b484f8
RoKofler@gmail.com <RoKofler@gmail.com@2fde9a34-d3d4-ce25-bbd2-7defdbab305e>
2012-02-13 16:44:35 +0000

    new output format gene-names are now shown ro"

--- trunk/gowinda/analysis/GOResultForCandidateSnp.java
+++ trunk/gowinda/analysis/GOResultForCandidateSnp.java
@@ -1,5 +1,6 @@
 package gowinda.analysis;
 
+import java.util.*;
 /*
  * Immutable container
  */
@@ -9,14 +10,21 @@ public class GOResultForCandidateSnp {
 	private final int observedCount;
 	private final double expectedCount;
 	private final double adjustedSignificance;
+	private final ArrayList<String> geneids;
 	public GOResultForCandidateSnp(GOEntry entry, double significance, double adjustedSignificance,int observed, double expected)
 	{
+		this(entry,significance,adjustedSignificance,observed,expected,new ArrayList<String>());
+	}
+	
+	public GOResultForCandidateSnp(GOEntry entry, double significance, double adjustedSignificance,int observed, double expected,ArrayList<String> geneids)
+	{
+		this.geneids=new ArrayList<String>(geneids);
 		this.goe=entry;
 		this.observedCount=observed;
 		this.expectedCount=expected;
 		this.adjustedSignificance=adjustedSignificance;
 		this.significance=significance;
-	}
+	} 
 	
 	public double significance()
 	{
@@ -39,10 +47,20 @@ public class GOResultForCandidateSnp {
 	{
 		return this.adjustedSignificance;
 	}
+	public ArrayList<String> geneids()
+	{
+		return new ArrayList<String>(this.geneids);
+	}
+	
+	public GOResultForCandidateSnp setGeneids(ArrayList<String> geneids)
+	{
+		return new GOResultForCandidateSnp(this.goe,this.significance,this.adjustedSignificance,this.observedCount,this.expectedCount,geneids);
+	}
 	
 	public GOResultForCandidateSnp setAdjustedSignificance(double adjustedSignificance)
 	{
 		return new GOResultForCandidateSnp(this.goe,this.significance,adjustedSignificance,this.observedCount,this.expectedCount);
 	}
+
 	
 }
